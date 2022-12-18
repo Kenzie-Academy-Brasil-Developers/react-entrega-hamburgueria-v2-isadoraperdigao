e75b9ace79965756457fb82538d009c36cf36278
Isadora <dorisperdigao@gmail.com>
2022-12-18 15:05:32 -0300

    feature: creates globalStyles, reset css and finishes slogan style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { BrandLogo } from './components/BrandLogo';
 import { Slogan } from './components/Slogan';
+import GlobalStyles from './styles/globalStyles';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <BrandLogo isDashboard={true}/>
       <Slogan />
     </div>
