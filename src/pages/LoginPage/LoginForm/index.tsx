d1ebc2e75409d4b751d6050cda2af7912ef81fc0
Isadora <dorisperdigao@gmail.com>
2022-12-18 23:34:10 -0300

    feature: create login and register functionalities

--- src/pages/LoginPage/LoginForm/index.tsx
+++ src/pages/LoginPage/LoginForm/index.tsx
@@ -6,14 +6,27 @@ import { BrandButton } from "../../../components/BrandButton"
 import { FormInput } from "../../../components/FormInput"
 import { GrayButton } from "../../../components/GrayButton"
 import { StyledFormParagraph } from "./style"
+import { useContext } from "react"
+import { iDataLoginParameter, UserContext } from "../../../providers/UserContext"
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import { loginSchema } from "./loginSchema"
 
 export const LoginForm = () => {
+    const {userLogin} = useContext(UserContext)
+    const {register, handleSubmit, formState: {errors}} = useForm<iDataLoginParameter>({
+        resolver: yupResolver(loginSchema),
+        mode: "onBlur",
+    })
+
     return (
-        <StyledFormContainer>
+        <StyledFormContainer onSubmit={handleSubmit(userLogin)}>
             <StyledTitle>Login</StyledTitle>
             <StyledForm>
-                <FormInput text="Nome" type="text"/>
-                <FormInput text="Senha" type="password" />
+                <FormInput text="Email" type="text" {...register("email")}/>
+                {errors.email && <p>{errors.email.message}</p>}
+                <FormInput text="Senha" type="password" {...register("password")}/>
+                {errors.password && <p>{errors.password.message}</p>}
                 <BrandButton text="Logar" isDashboard={false} type="submit" />
                 <StyledFormParagraph>Crie sua conta para saborear muitas delícias e matar sua fome!</StyledFormParagraph>
                     <Link to={"/register"}>
--- src/pages/RegisterPage/RegisterForm/index.tsx
+++ src/pages/RegisterPage/RegisterForm/index.tsx
@@ -1,24 +1,39 @@
+import { yupResolver } from "@hookform/resolvers/yup"
+import { useContext } from "react"
+import { useForm } from "react-hook-form"
 import { Link } from "react-router-dom"
 import { FormInput } from "../../../components/FormInput"
 import { GrayButton } from "../../../components/GrayButton"
+import { iDataLoginParameter, iDataRegisterParameter, UserContext } from "../../../providers/UserContext"
 import { StyledFormContainer } from "../../../styles/formContainer"
+import { StyledForm } from "../../../styles/formStyle"
 import { StyledTitle } from "../../../styles/typography"
+import { registerSchema } from "./registerSchema"
 import { StyledRegisterFormHeader, StyledRegisterSpan } from "./style"
 
 export const RegisterForm = () => {
+    const {userRegister} = useContext(UserContext)
+    const {register, handleSubmit, formState: {errors}} = useForm<iDataRegisterParameter>({
+        resolver: yupResolver(registerSchema),
+        mode: "onBlur",
+    })
+
+
     return (
-        <StyledFormContainer>
+        <StyledFormContainer onSubmit={handleSubmit(userRegister)}>
             <StyledRegisterFormHeader>
                 <StyledTitle>Cadastro</StyledTitle>
                 <Link to={"/"}>
                     <StyledRegisterSpan>Retornar para o login</StyledRegisterSpan>
                 </Link>
             </StyledRegisterFormHeader>
-            <FormInput text="Nome" type="text" />
-            <FormInput text="Email" type="email" />
-            <FormInput text="Senha" type="password" />
-            <FormInput text="Confirmar senha" type="password" />
-            <GrayButton text="Cadastrar" isDashboard={false} type="submit" />
+            <StyledForm>
+                <FormInput text="Nome" type="text" {...register("name")} />
+                <FormInput text="Email" type="email" {...register("email")} />
+                <FormInput text="Senha" type="password" {...register("password")} />
+                <FormInput text="Confirmar senha" type="password" />
+                <GrayButton text="Cadastrar" isDashboard={false} type="submit" />
+            </StyledForm>
         </StyledFormContainer>
     )
 }
